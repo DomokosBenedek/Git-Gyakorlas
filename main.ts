0474c3b4015cb5aab01ebe1c02c4db487a7f15c6
DomokosBenedek <benedekdomokos50@gmail.com>
2024-01-31 13:40:42 +0100

    2. feladat (szam beolvasas és listázás)

---
+++ main.ts
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let szamok: number[] = [];
+    let hozzaad = document.getElementById('hozzaad') as HTMLInputElement;
+    hozzaad.addEventListener("click", () => {
+        let szam = parseInt((document.getElementById('szam')  as HTMLInputElement).value); 
+        szamok.push(szam);
+    })
+})    
