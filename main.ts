496d4c164f825fb732b851e2aa55d55fd1125be4
DomokosBenedek <benedekdomokos50@gmail.com>
2024-01-31 13:56:50 +0100

    3. feladat (kiíras)

--- main.ts
+++ main.ts
@@ -5,5 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
         let szam = parseInt((document.getElementById('szam')  as HTMLInputElement).value); 
         szamok.push(szam);
     })
-    
+    let div = document.getElementById('szamok') as HTMLElement;
+    let p = document.createElement("p");
+    let szoveg = "";
+    szamok.forEach(element => {
+        szoveg = szoveg + "; " + element;
+    })
+    p.textContent = szoveg;
+    div.appendChild(p);
 })    
